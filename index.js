61bd52e68355f3d7257ac154ab4425ef314561d1
Aviv Nur <avivalviann@gmail.com>
2024-01-15 15:23:31 -0500

    add moon and night sky

--- index.js
+++ index.js
@@ -8,12 +8,79 @@ let svg = canvas.append('svg')
                 .attr('width', width)
                 .attr('style', 'background-color: lightblue');
 
+                // The sky night
+svg.append("rect").attr("x", 0)
+                    .attr("y", 0)
+                    .attr("width", width)
+                    .attr("height", height - height/3)
+                    .attr("fill", "lightblue")
+                    .transition()
+                    .duration(10000)
+                    .attr("fill", "black");
+
+// The Moon
+svg.append("circle").attr("cx", 90)
+                    .attr("cy", 90)
+                    .attr("r", 40)
+                    .attr("stroke", "white")
+                    .attr("fill", "white");
+
 // The sun
 svg.append("circle").attr("cx", 90)
                     .attr("cy", 90)
                     .attr("r", 40)
                     .attr("stroke", "yellow")
-                    .attr("fill", "orange");
+                    .attr("fill", "orange")
+                    .transition()
+                    .duration(10000)
+                    .attr("cx", 410)
+                    .attr("cy", 410)
+                    .attr("r", 0)
+                    .attr("stroke", "orange")
+                    .attr("fill", "red");
+
+// The stars
+svg.append("circle").attr("cx", 400)
+                    .attr("cy", 100)
+                    .attr("r", 1)
+                    .attr("fill", "white")
+                    .transition()
+                    .duration(10000)
+                    .attr("fill", "yellow");
+
+svg.append("circle").attr("cx", 450)
+                    .attr("cy", 150)
+                    .attr("r", 1)
+                    .attr("fill", "white")
+                    .transition()
+                    .duration(10000)
+                    .attr("fill", "yellow");
+
+svg.append("circle").attr("cx", 480)
+                    .attr("cy", 200)
+                    .attr("r", 1)
+                    .attr("fill", "white")
+                    .transition()
+                    .duration(10000)
+                    .attr("fill", "yellow");
+
+svg.append("circle").attr("cx", 200)
+                    .attr("cy", 200)
+                    .attr("r", 0.5)
+                    .attr("fill", "white")
+                    .transition()
+                    .duration(10000)
+                    .attr("r", 2)
+                    .attr("fill", "yellow");
+
+svg.append("circle").attr("cx", 200)
+                    .attr("cy", 150)
+                    .attr("r", 0.5)
+                    .attr("fill", "white")
+                    .transition()
+                    .duration(10000)
+                    .attr("r", 2)
+                    .attr("fill", "yellow");
 
 // The grass
 svg.append("rect").attr("x", 0)
@@ -45,7 +112,8 @@ svg.append("rect").attr("x", 367.5)
                   .attr("y", height - height/3 - 95)
                   .attr("width", 25)
                   .attr("height", 10)
-                  .attr("fill", "black");
+                  .attr("fill", "grey")
+                  .attr("stroke", "white");
 
 // The roof
 svg.append("polygon").attr("points", "300,300 420,300 400,260 320,260")
@@ -65,8 +133,10 @@ svg.append("rect").attr("x", 365)
                   .attr("width", 25)
                   .attr("height", 25)
                   .attr("stroke", "white")
-                  .attr("fill", "black");
-
+                  .attr("fill", "black")
+                  .transition()
+                  .duration(10000)
+                  .attr("fill", "yellow");
 
 // The door knob
 svg.append("circle").attr("cx", 328)
@@ -88,7 +158,10 @@ svg.append("line").attr("x1", 50)
                   .attr("x2", 50)
                   .attr("y2", height - height/3 - 100)
                   .attr("stroke", "black")
-                  .attr("stroke-width", 3);
+                  .attr("stroke-width", 3)
+                  .transition()
+                  .duration(10000)
+                  .attr("stroke", "grey");
 
 // The Indonesian flag
 svg.append("rect").attr("x", 52)
@@ -102,3 +175,4 @@ svg.append("rect").attr("x", 52)
                     .attr("width", 40)
                     .attr("height", 10)
                     .attr("fill", "white");
+
